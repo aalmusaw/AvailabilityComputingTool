b7b067bb8c5045c9b26a4f8049838dfb4c971395
Ali Al-Musawi <54688322+aalmusaw@users.noreply.github.com>
2021-09-15 16:39:56 -0400

    Add files via upload

---
+++ tests/test_interval.py
@@ -0,0 +1,48 @@
+import unittest
+from ..modules.interval import Interval
+
+class TestInterval(unittest.TestCase):
+    def testInit(self):
+        interval = Interval(1.0, 2.0)
+        self.assertEqual(1.0, interval.getLow())
+        self.assertEqual(2.0, interval.getHigh())
+        interval2 = Interval(interval)
+        self.assertEqual(1.0, interval2.getLow())
+        self.assertEqual(2.0, interval2.getHigh())
+
+    def testIsEmpty(self):
+        interval = Interval(1.0, 2.0)
+        self.assertFalse(interval.isEmpty())
+        interval = Interval(1.0, 0.0)
+        self.assertTrue(interval.isEmpty())
+
+    def testIntersectWithEmpty(self):
+        empty = Interval.getEmptyInterval()
+        other = Interval(2.0, 3.0)
+        self.assertEqual(empty, empty.intersect(other))
+        self.assertEqual(empty, other.intersect(empty))
+    
+    def testIntersectWithInfinity(self):
+        infinite = Interval.getInfiniteInterval()
+        other = Interval(2.0, 3.0)
+        self.assertEqual(other, infinite.intersect(other))
+        self.assertEqual(other, other.intersect(infinite))
+    
+    def testIntersectWithAny(self):
+        x = Interval(2.0, 3.0)
+        y = Interval(1.0, 3.0)
+        z = Interval(4.0, 5.0)
+        w = Interval(2.5, 4.0)
+        self.assertEqual(x, x.intersect(y))
+        self.assertTrue(x.intersect(z).isEmpty())
+        self.assertEqual(x.intersect(w), Interval(2.0, 4.0))
+
+    def testIn(self):
+        x = Interval(2.0, 3.0)
+        y = Interval(1.0, 3.0)
+        z = Interval(2.0, 2.5)
+        self.assertTrue(z in x)
+        self.assertFalse(y in x)
+    
+if __name__ == '__main__':
+    unittest.main()
